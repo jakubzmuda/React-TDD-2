175ccb13efedebe5eb0e7a0797e3f4194560c956
Michal.Witkowski <batoponton@gmail.com>
2019-04-02 07:33:56 +0200

    random working for app

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchRandomActivity } from '../actions/actions';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {addActivityToList, fetchRandomActivity} from '../actions/actions';
 
 class Activities extends Component {
   render() {
     return (
       <div>
-        <button data-test="random-activity-button" onClick={() => this.props.fetchRandomActivity()}>Random activity
+        <button
+          data-test="random-activity-button"
+          onClick={this.props.fetchRandomActivity}>Random activity
         </button>
         {this.renderActivity()}
       </div>
@@ -15,20 +17,18 @@ class Activities extends Component {
 
   renderActivity() {
     if (this.props.activityEntry) {
-      return <div data-test="activity-entry">{this.props.activities.name}</div>
+      return <div data-test="activity-entry">{this.props.activityEntry.activity}</div>
     }
     return <div data-test="activity-placeholder">Plz click the button</div>
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    activityEntry: state.activities.entry
-  }
-};
+const mapStateToProps = (state) => ({
+  activityEntry: state.activities.entry
+});
 
-const mapDispatchToProps = () => ({
-  fetchRandomActivity
+const mapDispatchToProps = (dispach) => ({
+  fetchRandomActivity: (payload) => dispach(fetchRandomActivity(payload))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Activities)
